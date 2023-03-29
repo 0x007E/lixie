589797f6cc5c315806275b9c42a79c721bc7b97d
sheery <117809130+sumsir22@users.noreply.github.com>
2023-03-29 14:20:44 +0200

    USART Update
    USART Interface finished

--- Firmware/Lixie_Firmware/02_Lixie_Clock_ATtiny406_141222/main.c
+++ Firmware/Lixie_Firmware/02_Lixie_Clock_ATtiny406_141222/main.c
@@ -15,9 +15,9 @@
 #include "clock/clock.h"
 
                               // t   Hell. R.   G.   B.
-volatile Clock_time_t hours   = {12, 0xFF, 255, 0, 0};
-volatile Clock_time_t minutes = {0, 0xFF, 0, 255, 0};
-volatile Clock_time_t seconds = {0, 0xFF, 0, 0, 255};
+volatile Clock_time_t hours   = {9, 0xFF, 255, 255, 255};
+volatile Clock_time_t minutes = {11, 0xFF, 255, 255, 255};
+volatile Clock_time_t seconds = {0, 0xFF, 255, 255, 255};
 
 volatile unsigned char miliseconds = 0;
 
@@ -82,7 +82,7 @@ int main(void)
    // Gewünschte Farbe
    
    _delay_ms(5000);
-   printf("\nPress key for setup\n\r");
+   printf("\n\n\n\rPress key for setup\n\r");
    
    char data = 0;
    
@@ -93,47 +93,119 @@ int main(void)
       {
          
          unsigned int mode = 0;
+         unsigned int segment = 0;
+         unsigned int status_settings = 1;
+         unsigned int status_color = 1;
+         unsigned int brightness = 31;
          
-         unsigned int hour = 0;
-         unsigned int minute = 0;
+         unsigned int hour   = 9;
+         unsigned int minute = 11;
          unsigned int second = 0;
          
-         unsigned int r = 0;
-         unsigned int g = 0;
-         unsigned int b = 0;
+         typedef struct
+         {
+            unsigned int R;
+            unsigned int G;
+            unsigned int B;
+         } Clock_Color_t;
+         
+         Clock_Color_t hour_setting   = {255,255,255};
+         Clock_Color_t minute_setting = {255,255,255};
+         Clock_Color_t second_setting = {255,255,255};
          
          // Run setup:
-         printf("\n\n\r1. Set time     2. Set Color     3. Exit\n\rSelect Mode: ");
-         scanf("%2u", &mode);
-         switch(mode)
+         
+         
+         do
          {
-            case 1:
-            do
+            printf("\n\n\r1.Time     2.Color   3.Brightness\n\rAny other Number: Exit\n\n\rBR: %u\n\rTime: %u:%u:%u\n\n\rSelect Mode: ", 
+            brightness, hour, minute, second);
+            
+            scanf("%2u", &mode);
+            
+            switch(mode)
             {
-               printf("\n\nSet Time [Hours:Minutes:Seconds]: ");
-               scanf("%2u:%2u:%2u", &hour, &minute, &second);
-               printf("\r");
-            } while ((hour > 23) || (minute > 59) || (second > 59));
-            
-            break;
+               case 1:
+               do
+               {
+                  printf("\n\n\rSet Time [Hours:Minutes:Seconds]: ");
+                  scanf("%2u:%2u:%2u", &hour, &minute, &second);
+                  printf("\r");
+               } while ((hour > 23) || (minute > 59) || (second > 59));
+               
+               break;
 
-            case 2:
-            printf("\n\nSet Color: ");
-            scanf("%2u:%2u:%2u\n",&r, &g, &b);
-            break;
-
-            case 3: break;
-
-            default: printf("\n\nError\nPress reset button\n\n");
-            return 0;
-            
-         }
+               case 2:
+               
+               do
+               {
+                  status_color = 1;
+                  printf("\n\n\r1. Hours     2. Minutes     3. Seconds    4. Exit\n\rSelect Segment: ");
+                  scanf("%2u", &segment);
+                  switch(segment)
+                  {
+                     case 1:
+                     printf("\n\nSet Color for hours (max. 255) [Red:Green:Blue]: ");
+                     scanf("%3u:%3u:%3u", &hour_setting.R, &hour_setting.G, &hour_setting.B);
+                     break;
+                     
+                     case 2:
+                     printf("\n\nSet Color for minutes (max. 255) [Red:Green:Blue]: ");
+                     scanf("%3u:%3u:%3u", &minute_setting.R, &minute_setting.G, &minute_setting.B);
+                     break;
+                     
+                     case 3:
+                     printf("\n\nSet Color for seconds (max. 255) [Red:Green:Blue]: ");
+                     scanf("%3u:%3u:%3u", &second_setting.R, &second_setting.G, &second_setting.B);
+                     break;
+                     
+                     case 4:
+                     status_color = 0;
+                     break;
+                  }
+                  
+               } while (status_color);
+               
+               break;
+               
+               case 3:
+               do
+               {
+                  printf("\n\n\rBrightness (max. 31): ");
+                  scanf("%2u", &brightness);
+                  
+               } while (brightness > 31);
+               break;
+               
+               default:    
+               status_settings = 0;
+               break;
+                
+            }
+       
+         } while (status_settings);
+         
          
          // Parameter übernehmen
          hours.time = (unsigned char)hour;
          minutes.time = (unsigned char)minute;
          seconds.time = (unsigned char)second;
          
+         hours.R = (unsigned char)hour_setting.R;
+         hours.G = (unsigned char)hour_setting.G;
+         hours.B = (unsigned char)hour_setting.B;
+         
+         minutes.R = (unsigned char)minute_setting.R;
+         minutes.G = (unsigned char)minute_setting.G;
+         minutes.B = (unsigned char)minute_setting.B;
+         
+         seconds.R = (unsigned char)second_setting.R;
+         seconds.G = (unsigned char)second_setting.G;
+         seconds.B = (unsigned char)second_setting.B;
+         
+         hours.intensity   = (unsigned char)brightness;
+         minutes.intensity = (unsigned char)brightness;
+         seconds.intensity = (unsigned char)brightness;
          break;
       }
       _delay_ms(1000);
@@ -143,7 +215,7 @@ int main(void)
    timer_init();           // Timer initialisieren
    clock_init();
    sei();                  // Interrupt starten
-   printf("\n");
+   printf("\n\n");
    while (1)
    {
       clock_data(&hours, &minutes, &seconds);  
